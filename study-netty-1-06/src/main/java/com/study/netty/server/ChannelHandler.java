b49578ec0f34735f089bfd455e2421e2486961c0
fuguangwei <fuguangwei@xg.com>
2024-08-06 17:07:47 +0800

    NettyServer群发消息

---
+++ study-netty-1-06/src/main/java/com/study/netty/server/ChannelHandler.java
@@ -0,0 +1,15 @@
+package com.study.netty.server;
+
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+/**
+ * @author fuguangwei
+ * @date 2024-08-06
+ */
+public class ChannelHandler {
+
+    //用于存放用户Channel信息，也可以建立map结构模拟不同的消息群
+    public static ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+}
